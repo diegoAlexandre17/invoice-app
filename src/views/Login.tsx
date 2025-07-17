533fe57cbed7460a24e63f16365b0b281ef20662
Diego Alexandre <elalexdgr@gmail.com>
2025-07-16 21:12:05 -0300

    Update Login.tsx

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -27,15 +27,10 @@ const Login = () => {
         {/* Right Column - Login Form */}
         <div className="w-full lg:w-[40%] flex items-center justify-center bg-gray-50 p-12 sm:px-6 lg:px-8">
           <div className="max-w-md w-full space-y-8">
-            <div className="flex justify-between items-center">
-              <div>
-                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                  {t("auth.loginTitle")}
-                </h2>
-                <p className="mt-2 text-center text-sm text-gray-600">
-                  {t("auth.loginSubtitle")}
-                </p>
-              </div>
+            <div className="flex items-center justify-center">
+              <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+                {t("auth.loginSubtitle")}
+              </h2>
             </div>
 
             <form className="mt-8 space-y-6">
