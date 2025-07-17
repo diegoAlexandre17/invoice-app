aa1ed312639146a9da434f0b80af5c7391eeea12
Diego Alexandre <elalexdgr@gmail.com>
2025-07-16 21:46:25 -0300

    Update client.ts

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -2,4 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const client = createClient(supabaseUrl, supabaseAnonKey);
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing Supabase environment variables. Please check your .env file.');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
