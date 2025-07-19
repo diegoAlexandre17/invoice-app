2b027f2a4ac53e90c0c96af938965632ad6a2616
Diego Alexandre <elalexdgr@gmail.com>
2025-07-19 17:25:43 -0300

    Update RecoveryPassword.tsx

--- src/views/auth/RecoveryPassword.tsx
+++ src/views/auth/RecoveryPassword.tsx
@@ -113,6 +113,13 @@ const RecoveryPassword = () => {
                 </div>
               </div>
 
+              {/* Error general del formulario */}
+              {errors.root && (
+                <div className="bg-red-50 border border-red-200 rounded-md p-3">
+                  <p className="text-sm text-red-600">{errors.root.message}</p>
+                </div>
+              )}
+
               <Button
                 type="submit"
                 className="w-full"
